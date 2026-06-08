d0469e7434bc19743944584a9c57ef50a99f2d1d
João Martins <lucas-martinsbr@hotmail.com>
2026-06-08 23:14:55 +0000

    improvement: included lab07 exercise solved

--- lab07-microfrontends/static/carrinho.js
+++ lab07-microfrontends/static/carrinho.js
@@ -1,13 +1,27 @@
 const cart = { items: [], total: 0 };
+
 function updateCartUI() {
     const list = document.getElementById('cart-list');
     const totalEl = document.getElementById('cart-total');
-    list.innerHTML = cart.items.length === 0 ? 'Vazio' : '';
+
     cart.items.forEach(item => {
         const div = document.createElement('div');
         div.innerText = `${item.nome} - R$ ${item.preco}`;
         list.appendChild(div);
     });
+
+    list.innerHTML = cart.items.length === 0 ? 'Vazio' : '';
     totalEl.innerText = cart.total.toFixed(2);
 }
+
 updateCartUI();
+
+window.addEventListener('cart:add', (e) => {
+    const { nome, preco } = e.detail;
+    console.log(`[CARRINHO] Recebi notificação de adição: ${nome}`);
+
+    cart.total += preco;
+
+    cart.items.push({ nome, preco });
+    updateCartUI();
+});
--- lab07-microfrontends/static/catalogo.js
+++ lab07-microfrontends/static/catalogo.js
@@ -1,16 +1,30 @@
 async function renderCatalogo() {
-    const response = await fetch('/api/v1/catalogo');
+    const response = await fetch('/api/bff/home');
     const data = await response.json();
     const container = document.getElementById('vitrine-list');
+
     container.innerHTML = '';
-    data.produtos.forEach(p => {
+
+    data.vitrine.forEach(p => {
         const div = document.createElement('div');
         div.className = 'product';
-        div.innerHTML = `<span><strong>${p.nome}</strong> - R$ ${p.preco}</span><button onclick="adicionarAoCarrinho('${p.nome}', ${p.preco})">Adicionar</button>`;
+        div.innerHTML = `
+            <span><strong>${p.nome}</strong> - R$ ${p.preco}</span>
+            <span class="stock-tag">Status: ${p.estoque}</span></span>
+            <button onclick="adicionarAoCarrinho('${p.nome}', ${p.preco})">Adicionar</button>
+        `;
         container.appendChild(div);
     });
 }
+
 function adicionarAoCarrinho(nome, preco) {
     console.log(`[CATALOGO] Evento: Adicionar ${nome}`);
+
+    const event = new CustomEvent('cart:add', { 
+        detail: { nome, preco } 
+    });
+
+    window.dispatchEvent(event);
 }
+
 renderCatalogo();
